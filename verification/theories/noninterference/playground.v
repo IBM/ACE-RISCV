ccc99fa345a3924aa624f2ff165349967724e1d2
Lennard Gäher <l.gaeher@posteo.de>
2024-02-13 11:02:44 +0100

    add some notes from meeting with wojciech

--- verification/theories/noninterference/playground.v
+++ verification/theories/noninterference/playground.v
@@ -16,3 +16,14 @@ Definition something (p:process) (owns:loc->Prop) (declassified:loc->Prop)
       /\ fst (run p s1) = fst (run p s2).
 
 End wip.
+
+
+(** Notes from meeting with Wojciech:
+   - we may need to investigate the system model further
+   - in the above sketch, should the security monitor be modelled as a process as well?
+      + yes, because it's behavior should also not be influenced by the concrete data in the state it doesn't own
+        it may still access that memory, however.
+   - Note: the reason why the above definition works is that hardware mechanisms + page table setup ensure that everyone but the security monitor can only access the memory they own or that was declassified
+   - Problem with the model: in our system, ownership and declassification evolve over time and depend on the state
+   - Problem with the model: non-determinism
+ *)
