ddcb12a806b7b079bf94aed7c164539045573cf7
Wojciech Ozga <woz@zurich.ibm.com>
2024-04-10 09:09:57 -0500

    Adjusting the configuraiton
    Signed-off-by: Wojciech Ozga <woz@zurich.ibm.com>

--- security-monitor/src/core/initialization/mod.rs
+++ security-monitor/src/core/initialization/mod.rs
@@ -141,7 +141,7 @@ fn initialize_memory_layout(fdt: &FlattenedDeviceTree) -> Result<(ConfidentialMe
 fn initalize_security_monitor_state(
     confidential_memory_start: ConfidentialMemoryAddress, confidential_memory_end: *const usize,
 ) -> Result<(), Error> {
-    const NUMBER_OF_HEAP_PAGES: usize = 400 * 1024;
+    const NUMBER_OF_HEAP_PAGES: usize = 80 * 1024;
     // Safety: initialization order is crucial for safety because at some point we
     // start allocating objects on heap, e.g., page tokens. We have to first
     // initialize the global allocator, which permits us to use heap. To initialize heap
