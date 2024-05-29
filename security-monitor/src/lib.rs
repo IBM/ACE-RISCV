a7fbd4d9dd7055ebee046c81679f55d775c883c3
Wojciech Ozga <woz@zurich.ibm.com>
2024-05-29 19:44:44 +0200

    Satisfy rustc nightly with pointer_is_aligned_to declaration (#58)

--- security-monitor/src/lib.rs
+++ security-monitor/src/lib.rs
@@ -12,6 +12,7 @@
 #![feature(const_mut_refs)]
 // #![feature(const_refs_to_cell)]
 #![feature(pointer_is_aligned)]
+#![feature(pointer_is_aligned_to)]
 #![feature(result_option_inspect)]
 #![feature(pointer_byte_offsets)]
 // used for RefinedRust annotations
