4a3c092151db03ec6f183faa8275b3d8364976ee
Wojciech Ozga <woz@zurich.ibm.com>
2025-04-09 03:00:27 -0500

    fix bug in remote commands
    Signed-off-by: Wojciech Ozga <woz@zurich.ibm.com>

--- security-monitor/src/confidential_flow/handlers/symmetrical_multiprocessing/hfence_gvma_vmid.rs
+++ security-monitor/src/confidential_flow/handlers/symmetrical_multiprocessing/hfence_gvma_vmid.rs
@@ -22,10 +22,10 @@ impl RemoteHfenceGvmaVmid {
 }
 
 impl ConfidentialHartRemoteCommandExecutable for RemoteHfenceGvmaVmid {
-    fn execute_on_confidential_hart(&self, _confidential_hart: &mut ConfidentialHart) {
+    fn execute_on_confidential_hart(&self, confidential_hart: &mut ConfidentialHart) {
         // TODO: execute a more fine grained fence. Right now, we just clear all tlbs
         crate::core::architecture::riscv::fence::hfence_gvma();
-        // TODO: should we expose IPI interrupt to a hart or not?
+        self.ipi.execute_on_confidential_hart(confidential_hart);
     }
 
     fn is_hart_selected(&self, hart_id: usize) -> bool {
